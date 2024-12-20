866019495a246aff6bf2ffe619cf7870bd668dc7
john-de-kross <chukwujekwujohncross@gmail.com>
2024-12-20 11:18:31 +0100

    changes

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -6,7 +6,7 @@ function Login() {
             <div className="auth-container h-full m-auto w-full md:w-[45%] md:h-[70vh] bg-[#3E5879]">
                 <div className="flex w-full h-10 justify-center items-center app-name">
                     {/* <img src="/HiChat.png" alt="app logo" /> */}
-                    <h2 className="font-[900] text text-xl md:text-3xl">
+                    <h2 className="font-[900] text text-2xl md:text-3xl">
                         <span className="">H</span>
                         <span>i</span>
                         <span>C</span>
@@ -16,7 +16,7 @@ function Login() {
                     </h2>
                 </div>
                 <div className="flex-col justify-center place-items-center input-field w-full mt-14">
-                    <div className="flex flex-col justify-center relative w-[70%] full-name">
+                    <div className="flex flex-col justify-start w-full md:justify-center relative md:w-[70%] full-name">
                         <label className="absolute top-[-13px] text-sm  font-[300] text-white px-3" htmlFor="">Full Name</label>
                         <input className="bg-transparent w-full h-[45px] px-0.5 font-[500]
                          border-t-0 outline-none rounded-md text-gray-200 border-2 border-x-[#FFB200] border-y-[#FFB200]"
