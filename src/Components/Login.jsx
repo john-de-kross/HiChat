bb33e23e4f1245fc1ae7b8a7eca3ffe62aa9af17
john-de-kross <chukwujekwujohncross@gmail.com>
2024-12-23 08:43:15 +0100

    changes

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -7,7 +7,11 @@ function Login() {
             <div className="form-container w-full h-full bg-[#3E5879] md:w-[35%] md:h-[80vh]">
                 <div className="form-header flex justify-center items-center py-3 text-[#fff] font-[500] md:font-bold md:text-lg">
                     <h2>Welcome to HiChat</h2>
-                    
+                </div>
+                <div className="textArea flex flex-col px-3">
+                    <input className="w-[100%] h-[6vh]" type="text" required/>
+                    <label className="labelling">Full Name</label>
+
                 </div>
 
             </div>
