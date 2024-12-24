10d3ede374a8659f75c6550ee6f23ca278435b48
john-de-kross <chukwujekwujohncross@gmail.com>
2024-12-24 11:16:04 +0100

    changes

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -8,8 +8,8 @@ function Login() {
                     <h2>Welcome to <span>H</span>iChat</h2>
                 </div>
                 <div className="input-area w-full py-6">
-                    <div className="full_name inputs pl-7 relative w-full h-[70px]">
-                        <input className="w-[90%] bg-transparent rounded-lg h-full md:w-[50%]" type="text" required/>
+                    <div className="full_name inputs pl-2 relative w-full h-[60px]">
+                        <input className="w-[98%] bg-transparent rounded-3xl h-full md:w-[50%]" type="text" required/>
                         <label className="labelling">Full Name</label>
                         <p className="error"></p>
                     </div>
