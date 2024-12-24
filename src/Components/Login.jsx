8db6fc7df2a831a37caa55469a961893f049d2fb
john-de-kross <chukwujekwujohncross@gmail.com>
2024-12-24 16:18:14 +0100

    changes

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -3,12 +3,12 @@ import hide from './hide.png'
 function Login() {
     return(
         <div className="flex justify-center w-full h-full md:h-screen items-center">
-            <div className="form-container w-full h-screen bg-[#608BC1] md:w-[30%] md:h-[80vh]">
+            <div className="form-container w-full min-h-screen bg-[#608BC1] md:w-[30%] md:h-[80vh]">
                 <div className="heading-text flex justify-center text-white text-xl py-4 md:text-lg">
                     <h2>Welcome to <span>H</span>iChat</h2>
                 </div>
                 <div className="input-area w-full">
-                    <div className="full_name inputs pl-2 relative w-full h-[60px]">
+                    <div className="full_name inputs pl-2 relative w-full h-[50px]">
                         <input className="w-[98%] bg-transparent rounded-3xl h-full md:w-[50%]" type="text" required/>
                         <label className="labelling">Full Name</label>
                         <p className="error"></p>
@@ -18,12 +18,12 @@ function Login() {
                         <label className="labelling">Username</label>
                         <p className="error"></p>
                     </div>
-                    <div className="full_name inputs pl-2 relative w-full h-[60px]">
+                    <div className="full_name inputs pl-2 relative w-full h-[50px]">
                         <input className="w-[98%] bg-transparent rounded-3xl h-full md:w-[50%]" type="text" required/>
                         <label className="labelling">Email</label>
                         <p className="error"></p>
                     </div>
-                    <div className="full_name inputs pl-2 relative w-full h-[60px]">
+                    <div className="full_name inputs pl-2 relative w-full h-[50px]">
                         <input className="w-[98%] bg-transparent rounded-3xl h-full md:w-[50%]" type="password" required/>
                         <label className="labelling">Password</label>
                         <p className="error"></p>
