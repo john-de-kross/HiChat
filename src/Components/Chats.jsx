31313ce8442058ecc010e2f6b5dbd45b4962a704
john-de-kross <chukwujekwujohncross@gmail.com>
2024-12-27 00:50:41 +0100

    changes

--- src/Components/Chats.jsx
+++ src/Components/Chats.jsx
@@ -15,12 +15,7 @@ function Chats() {
             </div>
             <div className="search w-full md:flex md:justify-center md:items-center h-20 pl-4">
                 <input className="w-[95%] srch-input rounded-3xl md:w-[80%] px-1 h-14" type="text" placeholder="Search..."/>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" 
-                viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 h-5 w-5 absolute ml-0.5 top-[9%]">
-                <path strokeLinecap="round" strokeLinejoin="round" 
-                d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
-                </svg>
-
+               
             </div>
            
 
