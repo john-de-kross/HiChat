4222d6ca6a2ab79fd89c2745532cee332e5e19d4
john-de-kross <chukwujekwujohncross@gmail.com>
2024-12-26 23:00:55 +0100

    changes

--- src/Components/Initializing.jsx
+++ src/Components/Initializing.jsx
@@ -5,7 +5,7 @@ function Initialize() {
         <div className="flex flex-col justify-center place-items-center w-full min-h-screen bg-slate-950">
             <div className="chat-logo">
                 <svg xmlns="http://www.w3.org/2000/svg" 
-                fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 w-40 stroke-blue-500 h-40 md:w-60 fill-blue-700 md:h-60">
+                fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 w-44 stroke-blue-500 h-44 md:w-60 fill-blue-700 md:h-60">
                 <path strokeLinecap="round" strokeLinejoin="round"
                  d="M8.625 12a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 
                  0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75
