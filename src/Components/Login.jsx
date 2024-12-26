9e6820728fb19cd003eaa036d57b6e90b4986cbc
john-de-kross <chukwujekwujohncross@gmail.com>
2024-12-26 08:30:31 +0100

    changes

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import hide from './hide.png'
 function Login() {
     return(
         <div className="flex justify-center md:items-center w-full min-h-screen bg-slate-900">
@@ -7,14 +6,14 @@ function Login() {
                 <div className="flex justify-center  items-center font-medium text-white text-lg md:text-slate-950">
                     <h1>ChatGoons Login</h1>
                 </div>
-                <form className="space-y-4 py-6 px-4">
+                <form className="space-y-8 mt- py-6 md:px-4">
                     <div className="input-field">
                         <input className="email input h-[45px] outline-none rounded" type="email" required placeholder="Email"/>
                         <div className="err">
                             <p></p>
                         </div>
                     </div>
-                    <div className="input-field">
+                    <div className="input-field py-2">
                         <input className="password input h-[45px] outline-none rounded" type="password" required placeholder="Password"/>
                         <div className="err">
                             <p></p>
--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import hide from './hide.png'
 
 function SignUp() {
     return ( 
@@ -6,33 +7,34 @@ function SignUp() {
             <div>
                 <h1 className="text-white text-xl md:text-2xl">ChatGoons</h1>
             </div>
-            <div className="con w-full md:w-96 md:h-[72vh] md:bg-slate-700">
-                <form className="space-y-4 py-4 md:pl-7">
-                    <div className="input-field">
-                        <input className="w-full rounded outline-none h-[50px] md:w-[90%]" type="text" required />
-                        <label>Full Name</label>
+            <div className="con w-full md:w-96 md:h-[76vh] md:bg-slate-700">
+                <form className="space-y-4 py-9 md:pl-7">
+                    <div className="input-fieldy">
+                        <input className="w-full rounded-xl md:rounded outline-none h-[50px] md:w-[90%]" type="text" required />
+                        <label className="labelling">Full Name</label>
                         <div className="error">
                             <p></p>
                         </div>
 
                     </div>
-                    <div className="input-field">
-                        <input className="w-full outline-none rounded h-[50px] md:w-[90%]" type="text" required />
-                        <label>Username</label>
+                    <div className="input-fieldy">
+                        <input className="w-full outline-none rounded-xl md:rounded h-[50px] md:w-[90%]" type="text" required />
+                        <label className="labelling">Username</label>
                         <div className="error">
                             <p></p>
                         </div>
                     </div>
-                    <div className="input-field">
-                        <input className="w-full rounded outline-none h-[50px] md:w-[90%]" type="email" required />
-                        <label>Email</label>
+                    <div className="input-fieldy">
+                        <input className="w-full rounded-xl md:rounded outline-none h-[50px] md:w-[90%]" type="text" required />
+                        <label className="labelling email">Email</label>
                         <div className="error">
                             <p></p>
                         </div>
                     </div>
-                    <div className="input-field">
-                        <input className="w-full outline-none rounded h-[50px] md:w-[90%]" type="password" required />
-                        <label>Password</label>
+                    <div className="input-fieldy relative">
+                        <input className="w-full outline-none rounded-xl md:rounded h-[50px] md:w-[90%]" type="password" required />
+                        <label className="labelling">Password</label>
+                        <img className="absolute w-6 h-6 right-12 mt-3 cursor-pointer" src={hide} alt="" />
                         <div className="error">
                             <p></p>
                         </div>
