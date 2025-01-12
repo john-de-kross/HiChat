2739119cc19c8e273f126a048051537c89c55e9a
john-de-kross <chukwujekwujohncross@gmail.com>
2025-01-12 14:23:15 +0100

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import './App.css'
 import Login from './Components/Login'
 import SignUp from './Components/SignUp';
 import Initialize from './Components/Initializing'
-import Chats from './Components/Chats'
 import {Routes, Route} from 'react-router-dom'
 
 
