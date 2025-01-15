228c94f1a7c5e945d0d9f20a49b79e217d1fefcc
john-de-kross <chukwujekwujohncross@gmail.com>
2025-01-15 12:01:17 +0100

    changes

--- src/Components/Chats.jsx
+++ src/Components/Chats.jsx
@@ -41,8 +41,8 @@ function Chats() {
             <div className="chats text-lg font-[500] flex justify-center items-center w-full h-full">
               <h2>No chats yet. Start a new conversation</h2>
             </div>
-            <div className="fixed bottom-8 right-4"> 
-              <div className="flex justify-center items-center font-[500] text-white content w-10 h-10 bg-blue-600 rounded-full">
+            <div className="fixed bottom-10 right-4"> 
+              <div className="flex justify-center items-center font-[500] text-white content w-14 h-14 bg-blue-600 rounded-full">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                 </svg>
