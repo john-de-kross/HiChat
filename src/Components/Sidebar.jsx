0e5d66f0fd79f8c662689f13c1dede188d9ab9d6
john-de-kross <chukwujekwujohncross@gmail.com>
2025-01-15 19:39:35 +0100

    changes

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Sidebar() {
     return ( 
-        <div className="w-[50%] h-screen absolute z-[2000] bg-slate-100">
+        <div className="w-[70%] h-screen absolute z-[2000] bg-slate-100">
             Hello world
 
         </div>
