232ef0daf5c3283029ca3888111a2f1d25c020e7
john-de-kross <chukwujekwujohncross@gmail.com>
2025-01-17 13:01:02 +0100

    changes

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -31,43 +31,43 @@ function Sidebar() {
                 </div>
             </div>
             <div className={`flex flex-col gap-6 border-4 border-x-0 ${isDarkMode ? 'border-y-slate-600': 'border-y-slate-200'} py-4 px-4`}>
-                <div className="flex justify-start gap-4 text-lg font-[500]">
+                <div className="flex justify-start gap-4 text-base font-[300]">
                     <svg 
                     xmlns="http://www.w3.org/2000/svg" 
                     fill="none" 
                     viewBox="0 0 24 24" 
                     strokeWidth={1.5} 
                     stroke="currentColor" 
-                    className="size-6 w-7 h-7">
+                    className="size-6 w-6 h-6">
                     <path 
                     strokeLinecap="round" 
                     strokeLinejoin="round" 
                     d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                     </svg>
-                    <h2>Profile</h2>
+                    Profile
                 </div>
-                <div className="settings flex justify-start gap-4 text-lg font-[500]">
+                <div className="settings flex justify-start gap-4 text-base font-[300]">
                     <svg 
                     xmlns="http://www.w3.org/2000/svg" 
                     fill="none" 
                     viewBox="0 0 24 24" 
                     strokeWidth={1.5} 
                     stroke="currentColor" 
-                    className="size-6 w-7 h-7">
+                    className="size-6 w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.325.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 0 1 1.37.49l1.296 2.247a1.125 1.125 0 0 1-.26 1.431l-1.003.827c-.293.241-.438.613-.43.992a7.723 7.723 0 0 1 0 .255c-.008.378.137.75.43.991l1.004.827c.424.35.534.955.26 1.43l-1.298 2.247a1.125 1.125 0 0 1-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.47 6.47 0 0 1-.22.128c-.331.183-.581.495-.644.869l-.213 1.281c-.09.543-.56.94-1.11.94h-2.594c-.55 0-1.019-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 0 1-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 0 1-1.369-.49l-1.297-2.247a1.125 1.125 0 0 1 .26-1.431l1.004-.827c.292-.24.437-.613.43-.991a6.932 6.932 0 0 1 0-.255c.007-.38-.138-.751-.43-.992l-1.004-.827a1.125 1.125 0 0 1-.26-1.43l1.297-2.247a1.125 1.125 0 0 1 1.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.086.22-.128.332-.183.582-.495.644-.869l.214-1.28Z" />
                     <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                     </svg>
                     Settings
                 </div>
             </div>
-            <div className="flex flex-col gap-8 py-6 text-lg font-[500] px-6">
+            <div className="flex flex-col gap-8 py-6 text-base font-[300] px-6">
                 <div className="group flex gap-4 justify-start">
                     <svg xmlns="http://www.w3.org/2000/svg"
                      fill="none" 
                      viewBox="0 0 24 24" 
                      strokeWidth={1.5} 
                      stroke="currentColor" 
-                     className="size-6 w-7 h-7">
+                     className="size-6 w-6 h-6">
                     <path 
                     strokeLinecap="round" 
                     strokeLinejoin="round" 
@@ -82,7 +82,7 @@ function Sidebar() {
                     viewBox="0 0 24 24" 
                     strokeWidth={1.5} 
                     stroke="currentColor" 
-                    className="size-6 w-7 h-7">
+                    className="size-6 w-6 h-6">
                     <path 
                     strokeLinecap="round" 
                     strokeLinejoin="round" 
@@ -97,7 +97,7 @@ function Sidebar() {
                     viewBox="0 0 24 24" 
                     strokeWidth={1.5} 
                     stroke="currentColor" 
-                    className="size-6 w-7 h-7">
+                    className="size-6 w-6 h-6">
                     <path 
                     strokeLinecap="round" 
                     strokeLinejoin="round" 
@@ -112,7 +112,7 @@ function Sidebar() {
                     viewBox="0 0 24 24" 
                     strokeWidth={1.5} 
                     stroke="currentColor" 
-                    className="size-6 w-7 h-7">
+                    className="size-6 w-6 h-6">
                     <path strokeLinecap="round" 
                     strokeLinejoin="round" 
                     d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z" />
@@ -123,8 +123,8 @@ function Sidebar() {
                 </div>
                 <div className="mode flex gap-4 justify-start">
                     Dark mode
-                    <div onClick={handleMode} className={`w-14 h-7  rounded-2xl transition delay-300 duration-300 ${isDarkMode ? 'bg-green-500' : 'bg-slate-300'}`}>
-                        <div className={`w-7 h-7 rounded-full bg-slate-100 transition delay-300 duration-300 ${isDarkMode ? 'translate-x-7' : 'translate-x-0'}`}>
+                    <div onClick={handleMode} className={`w-10 mt-1 h-5 rounded-2xl transition delay-300 duration-300 ${isDarkMode ? 'bg-green-500' : 'bg-slate-300'}`}>
+                        <div className={`w-5 h-5 rounded-full bg-slate-100 transition delay-300 duration-300 ${isDarkMode ? 'translate-x-5' : 'translate-x-0'}`}>
                         </div>
                     </div>
                 </div>
